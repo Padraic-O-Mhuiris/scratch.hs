5177f3fe76baeee553a40ab5a54e16c3cc7796e4
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2021-10-04 15:23:56 +0100

    updated flake

--- Scratch.hs
+++ Scratch.hs
@@ -3,3 +3,14 @@ module Main where
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
 
+thing :: String
+thing = "thing"
+
+addOne :: Integer -> Integer
+addOne x = (+) x 1
+
+addTwo :: Integer -> Integer
+addTwo = addOne . addOne
+
+addThree :: Integer -> Integer
+addThree = addTwo . addOne
--- flake.nix
+++ flake.nix
@@ -18,18 +18,8 @@
                 ghcid = { };
                 haskell-language-server = { };
                 hlint = { };
-                ormolu = { };
+                brittany = { };
               };
-              ## ormolu that uses ImportQualifiedPost.
-              ## To use, remove ormolu from the shell.tools section above, and uncomment the following lines.
-              # buildInputs =
-              #   let
-              #     ormolu = pkgs.haskell-nix.tool compiler-nix-name "ormolu" "latest";
-              #     ormolu-wrapped = pkgs.writeShellScriptBin "ormolu" ''
-              #       ${ormolu}/bin/ormolu --ghc-opt=-XImportQualifiedPost $@
-              #     '';
-              #   in
-              #   [ ormolu-wrapped ];
             };
           };
         };
